1f46c03477b7ef51d2e8e21000bcb3a006c843d7
Christopher Martin <cgmartin@gmail.com>
2016-01-05 19:12:18 -0500

    Site updated: 2016-01-05 19:12:18

--- js/script.js
+++ js/script.js
@@ -57,22 +57,38 @@
   // Caption
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
+      if ($(this).parent().hasClass('image-link')) return;
 
       var alt = this.alt;
-
       if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="image-link"></a>');
     });
 
-    $(this).find('.fancybox').each(function(){
+    $(this).find('.image-link').each(function(){
       $(this).attr('rel', 'article' + i);
     });
   });
 
+  // Bootstrap table style
+  $('.article-entry table').each(function(i, table)  {
+    if ($(this).parent().hasClass('table-responsive')) return;
+    $(this).addClass('table');
+    $(this).wrap('<div class="table-responsive"></div>');
+  });
+
   if ($.fancybox){
-    $('.fancybox').fancybox();
+    $('.image-link').fancybox();
+  }
+  if ($.magnificPopup){
+    $('.article-entry .image-link').magnificPopup({type: 'image'});
+    $('.article-gallery').each(function() {
+      $(this).magnificPopup({
+        delegate: '.image-link',
+        type: 'image',
+        gallery: { enabled: true }
+      });
+    });
   }
 
 })(jQuery);
